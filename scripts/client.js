a448ee432e7229dbdb0d9796d8d85827165d6136
LeGend <98607285+LeGend077@users.noreply.github.com>
2025-06-07 16:45:19 +0530

    Add files via upload

---
+++ scripts/client.js
@@ -0,0 +1,32 @@
+const socket = io('http://localhost:5000', { transports: ["websocket"] });
+
+// Get and emit login data
+
+let loginData = [prompt('Enter Username:'), prompt('Enter ID:')];
+socket.emit('log-in', loginData);
+
+// wait for signal if login succeeded to proceed
+socket.on('log-in-result', data => {
+    if (data) {
+        app(data);
+    } else {
+        alert('Login Failed!');
+        window.location.replace('/');
+    }
+})
+
+// Handle the App
+function app(data) {
+    document.getElementById('main-heading').innerText += ' ' + data[0] + '!';
+}
+
+document.getElementById('send').addEventListener('click', () => {
+    let message = document.getElementById('message').value.replace(/\n/g, '<br>')
+    socket.emit('send', message)
+
+    document.getElementById('content').innerHTML += '<div>' + 'You: ' + message + '</div>'
+})
+
+socket.on('receive', message => {
+    document.getElementById('content').innerHTML += '<div>' + message[1] + ': ' + message[0] + '</div>'
+})
