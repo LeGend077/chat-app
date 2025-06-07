5e65b24bfd7674dff7fda6d1a434248626ef904c
LeGend <98607285+LeGend077@users.noreply.github.com>
2025-06-07 17:08:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,34 @@
-const io = require('socket.io')(5000);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server running');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const allowedUsers = [
     'admin'
